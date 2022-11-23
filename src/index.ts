03af5b638076a09bc5143101bd6d71244652d044
An Tran <antranprm@gmail.com>
2022-11-23 12:42:00 -0800

    Simplify SCSS & transforms, new splashes, favicon

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import "./index.scss";
 
 import Typed from "typed.js";
 
-new Typed("#cwo", {
+new Typed("#splash", {
   strings: [
     "Learning how to setup GPU pipelines...",
     "Debugging my personal death ray code...",
@@ -17,10 +17,20 @@ new Typed("#cwo", {
     "Voodoo programming...",
     "Printing out my code for Elon Musk...",
     "Wishing I have time for art instead...",
-    "Wishing I have time for art instead...",
     "Reaching to you about your car's extended warranty...",
     "Coming up with more funny splash texts...",
     "Becoming the best that there ever was...",
+    "Wishing for a thought-to-program mind reading machine...",
+    "Discarding the weakness of the flesh for the purity of the machine...",
+    "Adding gotos to my code and discovering velociraptors...",
+    "Trying to exit vim...",
+    "Programming using a magnetized needle and a steady hand...",
+    "Using butterflies, eddy currents, and cosmic rays...",
+    "Segmentation fault (core dumped)",
+    "Waiting for my code to compile...",
+    "Waiting to reach my Ballmer's peak...",
+    "Finding what DenverCoder9 saw on that fateful day...",
+    "Looking at the same manual page for the 10th time today..."
   ],
   typeSpeed: 20,
   backSpeed: 0,
